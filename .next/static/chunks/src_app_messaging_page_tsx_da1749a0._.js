298ad3ecd1b52dcee7349293cc3f20167842ec0e
ALAE <89132578+alae30@users.noreply.github.com>
2025-07-26 21:19:20 +0100

    Add .next/static/chunks/src_app_messaging_page_tsx_da1749a0._.js 1753561089297

---
+++ .next/static/chunks/src_app_messaging_page_tsx_da1749a0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_48d5c75e._.js",
+  "static/chunks/node_modules_3ad65126._.js"
+],
+    source: "dynamic"
+});
