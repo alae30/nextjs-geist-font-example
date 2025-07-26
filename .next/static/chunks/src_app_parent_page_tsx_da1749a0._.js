704abbcb88eadb5328d110ea5e61e1dff0c706ba
ALAE <89132578+alae30@users.noreply.github.com>
2025-07-26 21:19:08 +0100

    Add .next/static/chunks/src_app_parent_page_tsx_da1749a0._.js 1753561076946

---
+++ .next/static/chunks/src_app_parent_page_tsx_da1749a0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7e6de524._.js",
+  "static/chunks/node_modules_cd59d093._.js"
+],
+    source: "dynamic"
+});
